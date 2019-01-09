a3eaa512495a3599dac4e5e2e8adb550092bb23f
Jamie <18686403@users.noreply.github.com>
2019-01-09 22:56:15 +0000

    Local Storage

--- js/gallery.js
+++ js/gallery.js
@@ -3,4 +3,22 @@ function imgSelect(imgs) {
   var expandImg = document.getElementById("expanded-img");
 	expandImg.src = imgs.src;
   expandImg.parentElement.style.display = "block";
+  saveSelectedColour(imgs.getAttribute("src"));
+}
+
+function saveSelectedColour(colour) {
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("selectedColour", colour);
+  }
+}
+
+function retrieveSelectedColour() {
+  if (typeof(Storage) !== "undefined") {
+    var colour = localStorage.getItem("selectedColour");
+    if (colour !== null) {
+      var expandImg = document.getElementById("expanded-img");
+      expandImg.src = colour;
+      expandImg.parentElement.style.display = "block";
+    }
+  }
 }
